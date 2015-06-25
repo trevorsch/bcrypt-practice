3e868f3a86d8161e0676438050df93821488a7a2
Trevor Scheuermann <trevorsch1@gmail.com>
2015-06-25 12:01:12 -0600

    password hash works on matching password inputs by user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('monk')('localhost/hashing-demo');
+var bcrypt = require('bcryptjs');
 var userCollection = db.get('users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,11 +9,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next) {
+  if(req.body.pass === req.body.var_pass){
   userCollection.insert({
     user: req.body.user_name,
-    pass: req.body.pass,
-    passVar: req.body.var_pass
+    pass: bcrypt.hashSync(req.body.pass, 8),
   });
+}
   res.redirect('/');
 });
 
